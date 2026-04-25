69ffaa9cc228253b4596082984428ce8a2f64a4c
hjasier <asiaurre@gmail.com>
2026-04-25 12:19:33 +0200

    Revert "now using react + tailswindcss"
    This reverts commit 20b5ec914101376ec181df182ecd241437b85915.

--- pear-app/ui/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Esto es crucial para Pear porque carga los archivos localmente (file://)
-})
